0078b25b610f1314407e22120e73c08b4773b6d2
Cao Chi Hai <haicao2805@gmail.com>
2023-09-06 12:23:59 +0700

    fix contact us

--- src/components/SectionLayout.tsx
+++ src/components/SectionLayout.tsx
@@ -4,7 +4,7 @@ interface SectionLayoutProps {
 
 const SectionLayout: React.FC<SectionLayoutProps> = ({ children }) => {
   return (
-    <div className="h-full max-w-screen-app mx-auto flex items-center justify-center">{children}</div>
+    <div className="h-full w-app mx-auto flex items-center justify-center">{children}</div>
   );
 }
 
